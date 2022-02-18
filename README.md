47b4786bee5e6fd99775d3ddbd37fb1bc3a040aa
HBK <sammcowen@gmail.com>
2022-02-18 13:53:00 -0800

    trying to get the badges and links to work to correct license choices

--- README.md
+++ README.md
@@ -1,43 +1,57 @@
 
   # test
 
+  ![badge](https://img.shields.io/badge/license-ApacheLicense2.0-brightorange)
   
   ## Table of Contents
   
   *[Licensing](#Licensing)
+
   *[Description](#description)
+
   *[Installation](#Installation)
+
   *[Usage](#Usage)
+
   *[Contributions](#Contributions)
+
   *[Tests](#Tests)
+
   *[Questions](#Questions)
 
   ## Licensing:
  
-  ![badge](https://img.shields.io/badge/license-MITLicense-brightorange)
-  This application uses  MIT License.
-  For more information please visit the [license description](https://choosealicense.com/licenses/mit/).
+  This application uses  Apache License 2.0.
+
+  For more information please visit the [license description](https://choosealicense.com/licenses/apache-2.0/).
 
   ## Description
+
   test
 
   ## Installation
   Follow these steps to properly install this application:
+
   test
 
   ## Usage 
   Instructions for use:
+
   test
 
   ## Contributions
   If you would like to contribute to this project, please follow these guidelines: 
+
   test
 
   ## Tests 
   Instructions for testing this application:
+
   test
 
   ## Questions
   Any questions please reach out via 
+
   *Github: https://github.com/sammcowen
+  
   *Email: sammcowen@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,8 +5,7 @@ function generateMarkdown(data) {
   if(`${data.license}` === 'Apache License 2.0'){
     licenseOption = 'ApacheLicense2.0';
     licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
-  };
-  if (`${data.license}` === 'MIT License') {
+  } else if (`${data.license}` === 'MIT License') {
     licenseOption = 'MITLicense';
     licenseLink = 'https://choosealicense.com/licenses/mit/';
   };
@@ -17,46 +16,60 @@ function generateMarkdown(data) {
   return `
   # ${data.title}
 
+  ![badge](https://img.shields.io/badge/license-${licenseOption}-brightorange)
   
   ## Table of Contents
   
-  *[Licensing](#Licensing)
-  *[Description](#description)
-  *[Installation](#Installation)
-  *[Usage](#Usage)
-  *[Contributions](#Contributions)
-  *[Tests](#Tests)
-  *[Questions](#Questions)
+-[Licensing](#Licensing)
+
+-[Description](#description)
+
+-[Installation](#Installation)
+
+-[Usage](#Usage)
+
+-[Contributions](#Contributions)
+
+-[Tests](#Tests)
+
+-[Questions](#Questions)
 
   ## Licensing:
  
-  ![badge](https://img.shields.io/badge/license-${licenseOption}-brightorange)
-  This application uses  ${data.license}.
+  This application uses ${data.license}.
+
   For more information please visit the [license description](${licenseLink}).
 
   ## Description
+
   ${data.description}
 
   ## Installation
   Follow these steps to properly install this application:
+
   ${data.installation}
 
   ## Usage 
   Instructions for use:
+
   ${data.usage}
 
   ## Contributions
   If you would like to contribute to this project, please follow these guidelines: 
+
   ${data.contributions}
 
   ## Tests 
   Instructions for testing this application:
+
   ${data.test}
 
   ## Questions
   Any questions please reach out via 
-  *Github: https://github.com/${data.github}
-  *Email: ${data.email}
+
+-Github: https://github.com/${data.github}
+
+-Email: ${data.email}
 `;
 }
 
